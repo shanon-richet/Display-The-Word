869225930b8f3d6285342bb88c17356aacc801f0
Shanon <shanon.richet@gmail.com>
2021-10-14 16:24:29 +0200

    function is less longer

--- script.js
+++ script.js
@@ -2,32 +2,29 @@ const main= document.querySelector('main');
 
 function createWord(){
     let word= 'SALUT';
-        setTimeout(function(){
-            const letters= document.createElement('p');
-            letters.innerHTML= word[0]
-            main.appendChild(letters)
-            setTimeout(function(){
-                const letters= document.createElement('p');
-                letters.innerHTML= word[0+1]
-                main.appendChild(letters)  
-                setTimeout(function(){
-                    const letters= document.createElement('p');
-                    letters.innerHTML= word[0+2]
-                    main.appendChild(letters) 
-                    setTimeout(function(){
-                        const letters= document.createElement('p');
-                        letters.innerHTML= word[0+3]
-                        main.appendChild(letters)  
-                        setTimeout(function(){
-                            const letters= document.createElement('p');
-                            letters.innerHTML= word[0+4]
-                            main.appendChild(letters)  
-                        },2000)
-                    },2000) 
-                },2000)
-            },2000)
-        }, 2000)
+
+    for(let i = 0; i <= 5; i++){
+        var interval=setInterval(function(){
+        const letters= document.createElement('p');
+        letters.innerHTML= word[i++]
+        main.appendChild(letters)
+        interval = setInterval(createWord, 4000)
+    },1000)
+    if (letters === word.slice(0,5)){
+        clearInterval(letters)
+    }
+    if (letters.length === 5){
+        clearInterval()
+    }
 }
-setTimeout(createWord, 2000);
+}
+setTimeout(createWord, 10);
+
+
+
+
+
+
+
 
 
