c03e4a5bd693595ea683e28ab615b7ff555658e9
Shanon <shanon.richet@gmail.com>
2021-10-14 13:37:44 +0200

    letters of the word appear each 2 seconds

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const main= document.querySelector('main');
 
 function createWord(){
-    let word= 'Salut'
-    for(let i= 0; i<5 ; i++){
-    const letters= document.createElement('p');
-    letters.innerHTML= word[0+i];
-    main.appendChild(letters);
-    }
+    let word= 'SALUT';
+        setTimeout(function(){
+            const letters= document.createElement('p');
+            letters.innerHTML= word[0]
+            main.appendChild(letters)
+            setTimeout(function(){
+                const letters= document.createElement('p');
+                letters.innerHTML= word[0+1]
+                main.appendChild(letters)  
+                setTimeout(function(){
+                    const letters= document.createElement('p');
+                    letters.innerHTML= word[0+2]
+                    main.appendChild(letters) 
+                    setTimeout(function(){
+                        const letters= document.createElement('p');
+                        letters.innerHTML= word[0+3]
+                        main.appendChild(letters)  
+                        setTimeout(function(){
+                            const letters= document.createElement('p');
+                            letters.innerHTML= word[0+4]
+                            main.appendChild(letters)  
+                        },2000)
+                    },2000) 
+                },2000)
+            },2000)
+        }, 2000)
 }
+setTimeout(createWord, 2000);
 
-setTimeout(createWord, 1000)
 
